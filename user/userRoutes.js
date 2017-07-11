e7a1c0fe9bdb92b04f06a2e90a8a22268a223329
Pulak Chakraborty <pulak.chakraborty@motius.local>
2017-07-11 15:46:28 +0200

    User image upload - necessary changes in the controller and route to facilitate upload of user image

--- user/userRoutes.js
+++ user/userRoutes.js
@@ -4,10 +4,11 @@ function userRoutes(passport) {
 
     var userController = require('./userController');
     var router = require('express').Router();
+    var multiparty = require('connect-multiparty')({uploadDir: './tmp'});
 
 
     router.post('/login', userController.login);
-    router.post('/signup', userController.signup);
+    router.post('/signup', multiparty, userController.signup);
     router.post('/unregister', passport.authenticate('jwt', {session: false}),userController.unregister)
 
     return router;
